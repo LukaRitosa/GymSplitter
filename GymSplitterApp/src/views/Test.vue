e79aa9a13ee7122a7e3550883d2e20a707d4a9cf
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2025-07-21 16:24:30 +0200

    commit

--- GymSplitterApp/src/views/Test.vue
+++ GymSplitterApp/src/views/Test.vue
@@ -2,24 +2,107 @@
     import { RouterLink, useRouter } from 'vue-router'
     import { useUserStore } from '@/stores/userStore'
     import { ref } from 'vue'
-    import { signInWithEmailAndPassword } from 'firebase/auth'
-    import { auth } from '@/firebase.js'
+    import { updateDoc, doc } from "firebase/firestore"
+
 
 
     const router=useRouter()
     const userStore=useUserStore()
 
+    const pitanja=[
+        {
+            tekst:"Koliko slobodnog vremena u tjednu imaš?",
+            odgovori: [
+                {
+                    izbor: "1 dan",
+                    vrijednost: 0 
+                },
+                {
+                    
+                    izbor: "2 dana",
+                    vrijednost: 1
+                },
+                {
+                    izbor: "3 dana",
+                    vrijednost: 2
+                },
+                {
+                    izbor: "4 dana",
+                    vrijednost: 3
+                }, 
+                {
+                    izbor: "5 dana",
+                    vrijednost: 4
+                },
+                {
+                    izbor: "6-7 dana",
+                    vrijednost: 5
+                }
+            ]
+        },
+        {
+            tekst:"Koliko iskustva treniranja u teretani imaš?",
+            odgovori: [
+                {
+                    izbor: "Nikad nisam.",
+                    vrijednost: 0 
+                },
+                {
+                    
+                    izbor: "Manje od 3 mjeseca.",
+                    vrijednost: 1
+                },
+                {
+                    izbor: "Manje od godinu dana.",
+                    vrijednost: 2
+                },
+                {
+                    izbor: "1-2 godine.",
+                    vrijednost: 3
+                }, 
+                {
+                    izbor: "3-5 godina.",
+                    vrijednost: 4
+                },
+                {
+                    izbor: "5+ godina.",
+                    vrijednost: 5
+                }
+            ]
+        }
+    ]
+
+
+    await updateDoc(doc(db, "users", userCredential.user.uid), {
+        iskustvo: odgovori.value[0],
+        slobodnoVrijeme: odgovori.value[1]
+    })
+
+
+    const odgovori = ref(Array(pitanja.length).fill(null))
+
+
 </script>
 
 <template>
 
-    <div>
-        Koliko slobodnog vremena u tjednu imaš?
+    <div v-for="(pitanje, index) in pitanja">
+        <b>
+            {{ pitanje.tekst }}
+        </b>
+
+        <div v-for="(o, i) in pitanje.odgovori">
+            <input type="radio" v-model="odgovori[index]" :value="o.vrijednost" :name="'pitanje'+ index"/>
+            {{ o.izbor }}
+        </div>
     </div>
 
+
     <div>
-        Koliko iskustva u teretani imaš?
+        {{ odgovori[0] }}
     </div>
+
+    
 
 
 </template>
