727f8869354bc7602ced68721c6f4a78f43e301d
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2025-08-27 11:16:45 +0200

    commit

--- GymSplitterApp/src/views/UrediDan.vue
+++ GymSplitterApp/src/views/UrediDan.vue
@@ -36,32 +36,13 @@
             const userDocRef = doc(db, `users/${userStore.currentUser.uid}`)
             const userSnap = await getDoc(userDocRef)
             if (userSnap.exists()) {
-            trenutniSplitId.value = userSnap.data().trenutniSplit
+                trenutniSplitId.value = userSnap.data().trenutniSplit
             }
         } catch (error) {
             console.error("Greška pri dohvaćanju:", error)
         }
     }
-
-    const dohvatiDan = async () => {
-        try {
-
-            const splitRef = doc(db, `users/${userStore.currentUser.uid}/splits/${trenutniSplitId.value}`)
-            const splitSnap = await getDoc(splitRef)
-            if (splitSnap.exists()) {
-            const splitData = splitSnap.data()
-            
-            const odabraniDan = splitData.dani?.find(d => Number(d.dan) === Number(danId))
-            if (odabraniDan) {
-                danPodaci.value = odabraniDan
-                await dohvatiVjezbe(odabraniDan.vjezbe || [])
-            }
-            }
-        } catch (error) {
-            console.error("Greška pri dohvaćanju:", error)
-        }
-    }
-
+    
     const dohvatiVjezbe = async (vjezbeIds) => {
         try {
             const rez = []
@@ -76,13 +57,13 @@
 
                 if (vjezbaSnap.exists()) {
                     rez.push({
-                    id,
-                    naziv: vjezbaSnap.data().naziv,
-                    opis: vjezbaSnap.data().Opis,
-                    slika: vjezbaSnap.data().slika,
-                    glavni_misic: vjezbaSnap.data().glavni_misic,
-                    ostali_misici: vjezbaSnap.data().ostali_misici || [],
-                    brojSetova: danPodaci.value.setovi?.[id] || 1
+                        id,
+                        naziv: vjezbaSnap.data().naziv,
+                        opis: vjezbaSnap.data().Opis,
+                        slika: vjezbaSnap.data().slika,
+                        glavni_misic: vjezbaSnap.data().glavni_misic,
+                        ostali_misici: vjezbaSnap.data().ostali_misici || [],
+                        brojSetova: danPodaci.value.setovi?.[id] || 1
                     })
                 }
             }
@@ -94,6 +75,26 @@
         }
     }
 
+    const dohvatiDan = async () => {
+        try {
+
+            const splitRef = doc(db, `users/${userStore.currentUser.uid}/splits/${trenutniSplitId.value}`)
+            const splitSnap = await getDoc(splitRef)
+            if (splitSnap.exists()) {
+                const splitData = splitSnap.data()
+                
+                const odabraniDan = splitData.dani?.find(d => Number(d.dan) === Number(danId))
+                if (odabraniDan) {
+                    danPodaci.value = odabraniDan
+                    await dohvatiVjezbe(odabraniDan.vjezbe || [])
+                }
+            }
+        } catch (error) {
+            console.error("Greška pri dohvaćanju:", error)
+        }
+    }
+
+    
     const dohvatiSveVjezbe= async () => {
         try{
             const sve=[]
@@ -149,7 +150,7 @@
     const smanjiSetove = (vjezba) => {
         if (vjezba.brojSetova > 1) {
             vjezba.brojSetova--
-        promjena.value = true
+            promjena.value = true
         }
     }
 
@@ -289,84 +290,114 @@
 </script>
 
 <template>
-    <div class="flex">
+    
+    <div v-if="loading" class="min-h-screen flex flex-col items-center px-4 justify-center">
+        <img src="https://static.wixstatic.com/media/68315b_30dbad1140034a3da3c59278654e1655~mv2.gif"/>
+    </div>
+
+    <div v-if="!loading && !izbornik" class="flex justify-center items-center min-h-screen flex-col">
+        <h2 class="text-2xl font-bold mb-4">
+            {{ danPodaci.naziv || 'Nepoznat dan' }}
+        </h2>
+
+        <button class="border bg-red-600 text-white hover:bg-red-300 p-2 rounded" @click="izbornik=true">
+            Dodaj vježbu
+        </button>
         
-        <div v-if="loading">
-            <img src="https://static.wixstatic.com/media/68315b_30dbad1140034a3da3c59278654e1655~mv2.gif"/>
-        </div>
+        <div v-for="(grupa, misic) in grupiraneVjezbeUDanu" :key="misic" class="mb-6">
+            <h3 class="text-xl font-bold mb-2">{{ misic }}</h3>
 
-        <div v-if="!loading && !izbornik">
-            <h2 class="text-xl font-bold mb-4">{{ danPodaci.naziv || 'Nepoznat dan' }}</h2>
-            
-            <ul class="space-y-4">
-            <div v-for="(grupa, misic) in grupiraneVjezbeUDanu" :key="misic" class="mb-6" @click="detalji=true">
-                <h3 class="text-lg font-bold mb-2">{{ misic }}</h3>
-                <div v-for="v in grupa" :key="v.id" class="border rounded-lg p-3">
+            <div class="flex gap-4">
+                <div v-for="v in grupa" :key="v.id"
+                 class="w-60 text-center flex flex-col items-center border border-red-700 rounded p-4">
+
                     <h4 class="font-semibold">{{ v.naziv }}</h4>
-                    <img :src="v.slika" :alt="v.naziv" class="my-2 w-50 h-auto rounded">
-                    <div class="mt-2">
 
+                    <img :src="v.slika" :alt="v.naziv" class="my-2 w-max-50 h-auto rounded">
+
+                    <div class="mt-2 w-full">
                         <div v-if="detalji" @click="detalji=false">
-                            <p class="text-gray-700">{{ v.opis }}</p>    
-
-                            <p>Glavni mišić: {{ v.glavni_misic }}</p>
-                            <p v-if="v.ostali_misici?.length">
+                            <p class="text-gray-700">
+                                {{ v.opis }}
+                            </p>    
+                            <p>
+                                Glavni mišić: {{ v.glavni_misic }}
+                            </p>
+                            <p v-if="v.ostali_misici.length">
                                 Ostali mišići: {{ v.ostali_misici.join(', ') }}
                             </p>
                         </div>
 
-                        <p class="mt-2 font-medium">
-                            Broj setova: 
-                            <button class="border bg-red-500 text-white hover:bg-red-300 p-2" @click="smanjiSetove(v)">
+                        <p class="mt-2 flex items-center justify-center gap-2">
+                            <button class="border bg-red-500 text-white hover:bg-red-300 px-2 rounded" @click="smanjiSetove(v)">
                                 -
                             </button>
+
                             {{ v.brojSetova }}
-                            <button class="border bg-red-500 text-white hover:bg-red-300 p-2" @click="povecajSetove(v)">
+
+                            <button class="border bg-red-500 text-white hover:bg-red-300 px-2 rounded" @click="povecajSetove(v)">
                                 +
                             </button>
+                            
                         </p>
-                        <button class="border bg-black text-white hover:bg-red-800 p-2"
-                        @click="ukloniVjezbu(v.id)">
+
+                        <button class="mt-2 border bg-black text-white hover:bg-red-800 p-3 rounded" @click="ukloniVjezbu(v.id)">
                             Ukloni vježbu
                         </button>
                     </div>
                 </div>
             </div>
-            <button class="border bg-blue-500 text-white hover:bg-blue-300 p-2" @click="izbornik=true">
-                dodaj vježbu
-            </button>
-            </ul>
-            <button v-if="promjena" class="border bg-red-500 text-white hover:bg-red-300 p-2" @click="spremiPromjene">
-                spremi promjene
-            </button>
         </div>
 
-        <div v-if="izbornik && !loading">
-            <div>
-                <h3 class="text-xl font-bold mb-4">Odaberi vježbe</h3>
+        <button v-if="promjena" class="border bg-black text-white hover:bg-red-500 p-2 rounded" @click="spremiPromjene">
+            Spremi promjene
+        </button>
 
-                <RouterLink to="/UserVjezbaMaker" class="border bg-red-500 text-white hover:bg-red-300 p-2">
-                    Dodaj vlastitu
-                </RouterLink>
+        <button class="border bg-red-700 text-white hover:bg-red-300 p-2 rounded" @click="detalji=true" v-if="!detalji">
+            Prikaži detalje
+        </button>
 
-                <div v-for="(grupa, misic) in grupiraneVjezbe">
-                    <h4 class="font-bold"> {{ misic }}</h4>
-                    <div class="grid grid-cols-2 md:grid-cols-3 gap-3 mt-2">
-                        <div v-for="v in grupa" :key="v.id" class="border p-3 rounded cursor-pointer hover:bg-gray-50"
-                        @click="dodajVjezbu(v)">
-                            <img :src="v.slika" class="w-full h-24 object-cover mb-2">
-                            <p class="font-medium">{{ v.naziv }}</p>
-                        </div>
-                    </div>
+        <button class="border bg-red-700 text-white hover:bg-red-300 p-2 rounded" @click="detalji=false" v-else>
+            Sakrij detalje
+        </button>
+
+
+    </div>
+
+    <div v-if="izbornik && !loading" class="flex justify-center items-center min-h-screen flex-col">
+        <h3 class="text-2xl font-bold mb-4">
+            Odaberi vježbe
+        </h3>
+
+        <div class="flex gap-3 mb-6">
+            <button class="border bg-gray-500 text-white hover:bg-red-300 p-2 rounded" @click="izbornik=false">
+                Nazad
+            </button>
+
+            <RouterLink to="/UserVjezbaMaker" class="border bg-red-500 text-white hover:bg-red-300 p-2 rounded">
+                Dodaj vlastitu
+            </RouterLink>
+        </div>
+
+        <div v-for="(grupa, misic) in grupiraneVjezbe" :key="misic" class="mb-8 w-full text-center">
+            <h4 class="text-xl font-bold mb-2">{{ misic }}</h4>
+
+            <div class="flex gap-4 justify-center p-2">
+                <div v-for="v in grupa" :key="v.id" @click="dodajVjezbu(v)"
+                    class="w-60 text-center flex flex-col items-center border rounded p-4 hover:bg-red-200">
+                    
+                    <h5 class="font-semibold">
+                        {{ v.naziv }}
+                    </h5>
+                    <img :src="v.slika" class="my-2 w-50 h-auto rounded">
                 </div>
-
-                <button @click="izbornik = false" class="px-4 py-2 bg-gray-300 hover:bg-gray-400 rounded">
-                    Zatvori
-                </button>  
             </div>
         </div>
 
     </div>
+
+
+
   
 
 </template>
